0a9457c127310c6e9c1f155206744f2c5b4bfed6
kangcinho <kangcinho@gmail.com>
2025-10-06 14:33:49 +0800

    Simpel Chatbot with Open AI

---
+++ src/main/java/com/open/ai/chatbot/open/ai/ChatbotOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.open.ai.chatbot.open.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotOpenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotOpenAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/open/ai/chatbot/open/ai/ChatbotOpenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.ai.chatbot.open.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotOpenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
